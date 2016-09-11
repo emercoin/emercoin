c8645b3e7b809df215061ad8b93132ada8c0f2f8
Spencer Lievens <silkcoindeveloper@gmail.com>
2016-09-12 00:53:29 +0100

    Show coins as available once confirmed
    Confirmed but non-mature coins were not showing as available to be spent. Now once coins confirm they will show as spendable/available.

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -214,7 +214,7 @@ void TransactionRecord::updateStatus(const CWalletTx &wtx)
         }
     }
     // For generated transactions, determine maturity
-    else if(type == TransactionRecord::Generated || type == TransactionRecord::StakeMint)
+    else if(type == TransactionRecord::Generated) 
     {
         if (wtx.GetBlocksToMaturity() > 0)
         {
